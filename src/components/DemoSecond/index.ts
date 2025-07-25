c08bfb9c8a7fa42a433e6c8fdae79e21ab99a0f6
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-07-25 11:50:19 +0200

    add support loading components from folders

--- src/components/DemoSecond/index.ts
+++ src/components/DemoSecond/index.ts
@@ -1,6 +1,6 @@
-import { BaseController } from '../controllers/BaseController';
-import { IPipeline } from '../models/Pipeline';
-import { IResult, IStruct } from '../models/Types';
+import { BaseController } from '../../controllers/BaseController';
+import { IPipeline } from '../../models/Pipeline';
+import { IResult, IStruct } from '../../models/Types';
 
 /**
  * Simple HelloWorld component controller for testing pipeline functionality
