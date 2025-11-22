705295864ca5466b0382634e14c336424e5d218d
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-11-22 13:31:24 +0100

    update docs support

--- src/modules/help/index.ts
+++ src/modules/help/index.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { KzModule } from "../../shared/controllers/KzModule";
 import { IConfig } from "../../shared/models/Config";
 import { IDependency } from "../../shared/tools";
@@ -5,6 +7,15 @@ import cli from "./configs/cli.json";
 import mcp from "./configs/mcp.json";
 
 export class ComponentModule extends KzModule {
+    constructor(dependency?: any) {
+        super(dependency);
+        const pac = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../../package.json'), 'utf-8'));
+        this.metadata.version = pac.version;
+        this.metadata.author = pac.author;
+        this.metadata.license = pac.license;
+        this.metadata.uri = pac.homepage;
+    }
+
     public register(config: IConfig | null, opts?: any): Promise<Record<string, IDependency> | null> {
         return Promise.resolve((config?.type === 'mcp' ? mcp as Record<string, IDependency> : cli) as Record<string, IDependency>);
     }
