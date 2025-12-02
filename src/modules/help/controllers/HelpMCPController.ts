5a2c7ca89393f2d04cc667fc81737a70e97ce16b
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-12-02 22:29:42 +0100

    update mcp tools

--- src/modules/help/controllers/HelpMCPController.ts
+++ src/modules/help/controllers/HelpMCPController.ts
@@ -2,46 +2,47 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
 import path from "path";
 import { z } from "zod";
 import { FileService } from "../../../shared/services/FileService";
+import { CLIController } from "../../cli/controllers/CLIController";
 import { MCPController } from "../../mcp/controllers/MCPController";
 
 export class HelpMCPController extends MCPController {
 
     public async register(server: McpServer): Promise<void> {
-        // select tool
-        server.registerTool("kozen_tool_help",
-            {
-                description: "Get information about a specific Kozen tool or tool help content",
-                inputSchema: {
-                    name: z.string().describe("Kozen Tool name")
-                },
-            },
-            this.select.bind(this)
-        );
+
         // general information
         server.registerTool("kozen_help",
             {
-                description: "Get information about Kozen tools and usage",
+                description: "Get information about all Kozen tools/modules",
                 inputSchema: {}
             },
             this.list.bind(this)
         );
 
+        // select tool specific help
+        server.registerTool("kozen_module",
+            {
+                description: "Get information about a specific Kozen tool/module or tool help content",
+                inputSchema: {
+                    name: z.string().describe("Kozen Tool/Module name")
+                },
+            },
+            this.select.bind(this)
+        );
+
         this.srvFile = this.srvFile || await this.assistant?.resolve<FileService>('core:file');
     }
 
-    async select(args: { name: string, format?: string }, extra?: any) {
+    async select(args: { name: string, format?: string }) {
         const { name } = args;
         try {
             if (!name) {
                 throw new Error('Tool name is required for get operation');
             }
-            const dir = process.env.DOCS_DIR || path.resolve(__dirname, `../../${name}/docs`);
-            const text = await this.srvFile?.select(name, dir);
-
+            const ctl = await this.assistant?.get<CLIController>(`${name}:controller:cli`);
+            const text = await ctl?.help();
             if (!text) {
                 throw new Error(`Failed to resolve Tool content: ${name} in ${this.srvFile?.dir}`);
             }
-
             return {
                 content: [
                     {
@@ -66,11 +67,9 @@ export class HelpMCPController extends MCPController {
         try {
             const dir = process.env.DOCS_DIR || path.resolve(__dirname, '../docs');
             const text = await this.srvFile?.select('kozen', dir);
-
             if (!text) {
                 throw new Error(`Failed to resolve Kozen content in ${this.srvFile?.dir}`);
             }
-
             return {
                 content: [
                     {
